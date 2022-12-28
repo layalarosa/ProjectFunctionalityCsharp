4b61514e0b39e2a3eb3c2e63c741895034415d6d
layalarosa <layalarosa@gmail.com>
2022-12-28 13:56:28 -0400

    Add More Practice.

---
+++ PruebaTecnica/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PruebaTecnica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.- Invertir una cadena
+            string text = "pato";
+            string textResult = "";
+
+            // A.forma por medio de un for 
+
+            //for (int i= text.Length-1; i>=0; i--)
+            //{
+            //    textResult += text[i];
+            //}
+
+            // B.por medio del metodo Reverse
+
+            char[] chars = text.ToCharArray();
+            Array.Reverse(chars);
+            textResult = new string(chars);
+
+            Console.WriteLine(textResult);
+
+            // 2. Cuantas veces se repite un caracter
+
+            string text2 = "iausioAAduhsdjqaaaddsdsSJAK88192aasa";
+            char character = 'A';
+
+            int n = 0;
+
+            // A.
+
+            //foreach (var c in text2)
+            //{
+            //    if (c == character)
+            //        n++;
+            //}
+
+            // B.
+
+            n = text2.Where(c => c == character).Count();
+
+            Console.WriteLine($"se repite la letra {character} {n} veces");
+
+            // 3. Distancia de Hamming
+
+            string text3 = "patitosw";
+            string text4 = "paratosa";
+
+            int distance = 0;
+
+            if (text3.Length != text4.Length)
+                throw new Exception("Longitudes Distintas");
+
+            // A.
+
+            for (int i = 0; i < text3.Length; i++)
+            {
+                if (text3[i] != text4[i])
+                    distance++;
+            }
+
+            Console.WriteLine("Distancia es de: " + distance);
+
+            // 4. Contador de Palabras
+
+            string text5 = "  un   texto    que         tiene palabras   ";
+            int n2 = 0;
+
+            text5 = Regex.Replace(text, @"\s+", " ").Trim();
+            var words = text5.Split();
+            n2 = words.Length;
+
+            Console.WriteLine("Numero de palabras: " + n);
+
+            // 5. Contar Numeros
+
+            string text6 = "an1298jq???da%22121230001masA";
+            string pattern = @"[a-zA-Z";
+            var regex = new Regex(pattern);
+            int n3 = regex.Matches(text6).Count;
+
+            Console.WriteLine("cantidad: " + n3);
+
+
+        }
+    }
+}
---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una instancia del tamagotchi
+            Tamagotchi tama = new Tamagotchi("Pepito");
+
+            // Mostrar la felicidad inicial del tamagotchi
+            Console.WriteLine(tama.Happiness);  // 100
+
+            // Hacer que pase el tiempo y ver cómo cambian los valores
+            tama.TimePasses();
+            Console.WriteLine(tama.Hunger);  // 10
+            Console.WriteLine(tama.Happiness);  // 95
+            Console.WriteLine(tama.Sickness);  // 5
+
+            // Darle de comer al tamagotchi y ver cómo cambia su hambre
+            tama.Feed();
+            Console.WriteLine(tama.Hunger);  // 0
+        }
+    }
+
+    class Tamagotchi
+    {
+        public string Name { get; set; }
+        public int Hunger { get; set; }
+        public int Happiness { get; set; }
+        public int Sickness { get; set; }
+
+        public Tamagotchi(string name)
+        {
+            this.Name = name;
+            this.Hunger = 0;
+            this.Happiness = 100;
+            this.Sickness = 0;
+        }
+
+        public void Feed()
+        {
+            if (this.Hunger > 0)
+            {
+                this.Hunger -= 10;
+            }
+        }
+
+        public void Play()
+        {
+            if (this.Happiness < 100)
+            {
+                this.Happiness += 10;
+            }
+        }
+
+        public void GiveMedicine()
+        {
+            if (this.Sickness > 0)
+            {
+                this.Sickness -= 10;
+            }
+        }
+
+        public void TimePasses()
+        {
+            this.Hunger += 10;
+            this.Happiness -= 5;
+            this.Sickness += 5;
+        }
+    }
+
+    
+
+}
