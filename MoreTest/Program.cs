8c61250c1096a5901eb3d3b8c2674fd5475ef353
layalarosa <layalarosa@gmail.com>
2023-01-11 17:48:33 -0400

    More Practice Code.

--- MoreTest/Program.cs
+++ MoreTest/Program.cs
@@ -14,26 +14,47 @@ namespace MoreTest
                 {
                     Id = 1,
                     ProductName = "Mono",
+                },
+
+                new Product
+                {
+                    Id = 2,
+                    ProductName = "Sapo",
                 }
             };
 
-            //Get
+            //Get All
             Product MostrarProducto = new Product();
             for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine(list[i].Id);
                 Console.WriteLine(list[i].ProductName);
-
-                if (list[i].ProductName == "Mono")
-                {
-                    list[i].ProductName = "Mono";
-                    break;
-                }
-
             }
 
-            MostrarProducto.ProductName = "Mono";
-            Console.ReadKey();
+            //Get
+
+            //Post
+            // Esto no es una buena forma debido a el orm o la base de datos crea la id
+            Console.WriteLine("Inserte la Id del Producto");
+            var Id = Console.ReadLine();
+            Console.WriteLine("Inserte el nombre del Producto");
+            string ProductName = Console.ReadLine();
+
+            list.Add(new Product());
+
+            
+            for (int i = 0; i < list.Count; i++)
+            {
+                
+                Console.WriteLine(list[i].Id);
+                Console.WriteLine(list[i].ProductName);
+            }
+
+            //Put
+
+            //Delete
+
+
         }
     }
 
